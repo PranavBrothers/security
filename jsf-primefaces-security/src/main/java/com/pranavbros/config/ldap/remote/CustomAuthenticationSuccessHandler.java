4e1d0ad176918fef80b63d0d50eb5b9d91f948da
Rajkumar <monishrajkumar@gmail.com>
2020-11-21 22:23:38 +0530

    included jsp file for redirection

--- jsf-primefaces-security/src/main/java/com/pranavbros/config/ldap/remote/CustomAuthenticationSuccessHandler.java
+++ jsf-primefaces-security/src/main/java/com/pranavbros/config/ldap/remote/CustomAuthenticationSuccessHandler.java
@@ -1,14 +1,12 @@
 package com.pranavbros.config.ldap.remote;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.ldap.userdetails.LdapUserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -19,19 +17,22 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 			Authentication authentication) throws IOException, ServletException {
 
 		LdapUserDetails principal = (LdapUserDetails) authentication.getPrincipal();
-		System.out.println("principal" + principal.getUsername());
-		boolean isAdmin = false;
-		Iterator<? extends GrantedAuthority> grantedAuthorityIterator = principal.getAuthorities().iterator();
-		while (grantedAuthorityIterator.hasNext()) {
-			if (grantedAuthorityIterator.next().getAuthority().equalsIgnoreCase("ROLE_ADMIN")) {
-				isAdmin = true;
-			}
-		}
-		if (isAdmin) {
-			response.sendRedirect("/pages/admin.xhtml");
-		} else {
-			response.sendRedirect("/pages/user.xhtml");
-		}
+		//boolean isAdmin = false;
+		////Iterator<? extends GrantedAuthority> grantedAuthorityIterator = principal.getAuthorities().iterator();
+		//while (grantedAuthorityIterator.hasNext()) {
+		//	if (grantedAuthorityIterator.next().getAuthority().equalsIgnoreCase("ROLE_ADMIN")) {
+		//		isAdmin = true;
+		//	}
+		//}
+		
+		request.getSession().setAttribute("username", principal.getUsername());
+		response.sendRedirect("/index.jsp");
+		
+		//if (isAdmin) {
+		//	response.sendRedirect("/pages/admin.xhtml");
+		//} else {
+		//	response.sendRedirect("/pages/user.xhtml");
+		//}
 	}
 
 }
---
+++ jsf-primefaces-security/src/main/resources/META-INF/resources/index.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<html lang="en">
+<body>
+    <div>
+        <div>
+            <h1>Welcome </h1>
+            <h2>Hello ${username} ...</h2>
+             
+            Click on this <strong><a href="pages/add-car.xhtml">link</a></strong> to visit another page.
+        </div>
+    </div>
+</body>
+</html>
